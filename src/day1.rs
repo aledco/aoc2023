0c67b862008b73b3db8790e9827664d4dd009117
Alexander Cooper <alexanderecooper@gmail.com>
2024-12-07 16:03:13 -0700

    finished day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,4 @@
-pub fn p1(input: String) -> i64
+fn setup(input: String) -> (Vec<i64>, Vec<i64>)
 {
     let lines = input.split("\n").collect::<Vec<&str>>();
     let mut col1 = Vec::new();
@@ -18,6 +18,52 @@ pub fn p1(input: String) -> i64
         col2.push(cols[1]);
     }
 
-    // TODO sort cols, compute answer
-    (col1.len() + col2.len()).try_into().unwrap()
+    col1.sort();
+    col2.sort();
+
+    (col1, col2)
+}
+
+pub fn p1(input: String) -> i64
+{
+    let (col1, col2) = setup(input); 
+
+    let mut result: i64 = 0;
+    for (c1, c2) in col1.iter().zip(col2)
+    {
+        result += (c1 - c2).abs();
+    }
+    
+    result
+}
+
+pub fn p2(input: String) -> i64
+{
+    let (col1, col2) = setup(input); 
+
+    let mut result: i64 = 0;
+    let (mut i, mut j) = (0, 0);
+    while i < col1.len()
+    {
+        let c1 = col1[i];
+        while j < col2.len() && col2[j] < c1 
+        {
+            j += 1;
+        }
+
+        let mut count = 0;
+        while j < col2.len() && col2[j] == c1
+        {
+            count += 1;
+            j += 1;
+        }
+
+        while i < col1.len() && col1[i] == c1
+        {
+            result += c1 * count;
+            i += 1;
+        }
+    }
+
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
+use std::env;
+
 mod input;
 mod day1;
 
 fn main()
 {
-    let input = input::read(1);
-    let answer = day1::p1(input);
+    let args: Vec<String> = env::args().collect();
+    let (day, part) = parse_args(args);
+    let input = input::read(day);
+    let answer = match (day, part)
+    {
+        (1, 1) => day1::p1(input),
+        (1, 2) => day1::p2(input),
+        _ => panic!("invalid day/part")
+    };
+    
+    println!("day {} part {}", day, part);
     println!("{}", answer);
+}
+
+fn parse_args(args: Vec<String>) -> (i32, i32)
+{
+    if args.len() > 2
+    {
+        (args[1].parse::<i32>().expect("args must be integers"), args[2].parse::<i32>().expect("args must be integers"))
+    }
+    else if args.len() > 1
+    {
+        (args[1].parse::<i32>().expect("args must be integers"), 1)
+    }
+    else
+    {
+        (1, 1)
+    }
 }
